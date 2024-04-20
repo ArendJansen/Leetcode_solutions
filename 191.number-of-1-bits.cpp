740439190dc159019c799b755362fd198ef9b874
Arend Jansen <arendjansen8@gmail.com>
2024-04-20 23:45:45 +0200

    New solve

---
+++ 191.number-of-1-bits.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=191 lang=cpp
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int hammingWeight(int n) {
+
+        int res = 0;
+
+        while (n > 0){
+            if (n%2 != 0 ){
+                ++res;
+            }
+            n = n/2;
+        }
+
+        return res;
+        
+    }
+};
+// @lc code=end
+
