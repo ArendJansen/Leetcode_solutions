da1369d36cf19c32b5a93b7983141c918fbe5008
Arend Jansen <arendjansen8@gmail.com>
2024-04-21 23:47:57 +0200

    New solve

---
+++ 463.island-perimeter.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=463 lang=cpp
+ *
+ * [463] Island Perimeter
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int res =0;
+        int ncol = grid.size();
+        int nrow = grid[0].size();
+        for (int i =0; i < grid.size(); ++i){
+            for (int j =0; j < grid[i].size(); ++j){
+                if (grid[i][j] == 1){
+                    int sides = 4;
+                    if (i != 0 && grid[i-1][j] == 1){
+                        --sides;
+                    }
+                    if (i != ncol - 1 && grid[i+1][j] == 1){
+                        --sides;
+                    }
+                    if (j != 0 && grid[i][j-1] ==1){
+                        --sides;
+                    }
+                    if (j != nrow -1 && grid[i][j+1] == 1){
+                        --sides;
+                    }
+                    res += sides;
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
