2c3d229d4d717bdf3950bf6f00f10f12f8504c77
Arend Jansen <arendjansen8@gmail.com>
2024-05-05 13:39:39 +0200

    New solve

---
+++ 120.triangle.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=120 lang=cpp
+ *
+ * [120] Triangle
+ */
+
+// @lc code=start
+#include <vector>
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        if (triangle.size() == 1){
+            return triangle[0][0];
+        }
+
+        for (int i = triangle.size() -2; i >= 0; --i){
+            for (int j = 0; j < triangle[i].size(); ++j){
+                triangle[i][j] += std::min(triangle[i+1][j], triangle[i+1][j+1]);
+            }
+        }
+
+        return triangle[0][0];
+        
+    }
+};
+// @lc code=end
+
