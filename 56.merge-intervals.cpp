6c0e0e9753f0ec0e93500c65f2ebf4fc72ee8828
Arend Jansen <arendjansen8@gmail.com>
2024-05-07 19:25:32 +0200

    New solve

---
+++ 56.merge-intervals.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=56 lang=cpp
+ *
+ * [56] Merge Intervals
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        if (intervals.size() <= 1){
+            return intervals;
+        }
+
+        std::sort(
+            intervals.begin(), 
+            intervals.end(),
+            [](const std::vector<int>& a, const std::vector<int>&b){
+                return a[0] < b[0];
+            });
+
+        for (auto it = intervals.begin(); it != intervals.end() -1; ) {
+            if ( (*(it+1))[0] <= (*it)[1] ) {
+                (*it)[1] = std::max((*it)[1], (*(it+1))[1]);
+                ++it;
+                it = intervals.erase(it);
+                --it;
+            } 
+            else {
+                ++it;
+            }
+        }
+        return intervals;
+        
+    }
+};
+// @lc code=end
+
